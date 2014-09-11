b9b65d87ff501ec988a13a675b076bba6715b189
Zbigniew Probulski <zibi@implix.com>
2014-09-11 21:16:04 +0200

    user model validations checked

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do  
+  it "validates presence of email" do
+    expect(subject).to be_invalid
+    expect(subject.errors[:email]).to_not be_empty
+  end
+
+  it 'validates uniqueness of email' do
+    saved_user = create(:user, email: 'test@test.com')
+    subject.email = 'test@test.com'
+    expect(subject).to be_invalid    
+    expect(subject.errors[:email].join).to match /taken/
+  end
 end
