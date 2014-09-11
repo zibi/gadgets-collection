63a9de24e8780f09ce3a121d348c11f193505c6b
Zbigniew Probulski <zibi@implix.com>
2014-09-12 01:42:49 +0200

    specs for gadgets index view added

--- spec/views/gadgets/index.html.erb_spec.rb
+++ spec/views/gadgets/index.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "gadgets/index.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:gadget1) { create(:gadget) }
+  let(:gadget2) { create(:gadget) }
+  
+  before :each do
+    assign(:gadgets, [gadget1, gadget2])
+    render
+  end
+
+  it "displays all the gadgets" do
+    assert_select('a[href=?]', gadget_path(gadget1))
+    assert_select('a[href=?]', gadget_path(gadget2))
+  end
+  
+  it 'display link for adding new gadgets' do
+    assert_select('a[href=?]', new_gadget_path)    
+  end
 end
